59f9f93cad20bd28ba263d184bb52a4060d29886
Jose Moreno <morenojr90@yahoo.com>
2018-06-19 10:36:54 -0700

    Add email, password, confirm password, and button to activity_main, add SQLiteOpenHelper class

---
+++ app/src/main/java/me/josefmoreno/mvpexample/DatabaseHelper.java
@@ -0,0 +1,16 @@
+package me.josefmoreno.mvpexample;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ app/src/main/java/me/josefmoreno/mvpexample/MainActivity.java
@@ -0,0 +1,32 @@
+package me.josefmoreno.mvpexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1, e2, e3;
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.email);
+        e2=(EditText)findViewById(R.id.pass);
+        e3=(EditText)findViewById(R.id.cpass);
+        b1=(Button)findViewById(R.id.register);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                String s3 = e3.getText().toString();
+            }
+        });
+
+    }
+}
